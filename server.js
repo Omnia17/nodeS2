0393f83f6f1c34a795f20843146289f32b6e17de
omnia17 <98535336+Omnia17@users.noreply.github.com>
2023-02-14 15:57:56 +0100

    removed node_modules folder

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
 
-    var http = require ('http');
-    var url =require ('url');
-    var server = http.createServer( function (req, res) {
-        var page= url.parse(req.url).pathname;
-        console.log(page);
-     res.writeHead (200,{"content-Type":"text/html"}) ;
+   //  var http = require ('http');
+   //  var url =require ('url');
+   //  var server = http.createServer( function (req, res) {
+   //      var page= url.parse(req.url).pathname;
+   //      console.log(page);
+   //   res.writeHead (200,{"content-Type":"text/html"}) ;
 
 
 
-     if (page == '/') {
-        res.write ('Vous êtes dans la page d\'accueil');
-     }
-     else if (page == '/Contact') {
-        res.write ('Vous êtes dans la page Contact !');
-     }
-     else if (page == '/Affichage/1/user') {
-        res.write ('Affichez 1\'utilisateur qui a 1\*id 1 !');
-     }
-     else  {
-        //return{ status: 404, message:"not found"}
-        res.write ('404 not found !');
-     }
+   //   if (page == '/') {
+   //      res.write ('Vous êtes dans la page d\'accueil');
+   //   }
+   //   else if (page == '/Contact') {
+   //      res.write ('Vous êtes dans la page Contact !');
+   //   }
+   //   else if (page == '/Affichage/1/user') {
+   //      res.write ('Affichez 1\'utilisateur qui a 1\*id 1 !');
+   //   }
+   //   else  {
+   //      //return{ status: 404, message:"not found"}
+   //      res.write ('404 not found !');
+   //   }
     
     //  res.write ('<p>Voiciiiiiiii un paragraphe <strong>HTML</strong> !</p>');
     //  res.write('<!DOCTYPE html>'+
@@ -33,7 +33,33 @@
     //  '</body>'+
     //  '</html>');
    // res.write("bien on avance")
-     res.end();
-});
+    // res.end();
+//});
   
-   server.listen (8080);
+   //server.listen (8080);
+
+
+//    const express=require('express');
+// const app= express();
+
+
+// const logDateTime=(req, res,next)=>{
+//    req.dateTime=new Date();
+//    console.log(new Date ());
+//    next();
+
+// }
+// app.use(logDateTime)
+// const requireJsonContent = () =>{
+//    return (req,res,next)=> {
+//       if (req.headers['content-type']!=='application/json'){
+//       res.status(400).send('server requires application/json')
+//        } else {
+//          next();
+//        }
+//    }
+// }
+// app.get('/',requireJsonContent(), (req,res,next)=> {
+//    res.send(`hello 4 twin 5  at ${req.dateTime}`) ;
+// });
+// app.listen(8080);
